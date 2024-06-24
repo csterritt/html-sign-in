d3f16518d0e427533f9e35e83f060d754ee481e1
csterritt <csterritt@gmail.com>
2024-06-23 20:12:40 -0400

    Maybe proper tailwind minify for build.

--- renderer.tsx
+++ renderer.tsx
@@ -4,7 +4,7 @@ export const renderer = jsxRenderer(({ children, title }) => {
   return (
     <html>
       <head>
-        <link href='./style.css' rel='stylesheet' />
+        <link href='/static/style.css' rel='stylesheet' />
         <title>{title}</title>
       </head>
       <body class='m-auto font-sans'>{children}</body>
