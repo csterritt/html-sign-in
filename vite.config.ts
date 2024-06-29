2d70dcd7e3783f91521f8f42d1c8e3b6e148e2ff
csterritt <csterritt@gmail.com>
2024-06-29 11:23:54 -0400

    Trying handlebars via the vite-plugin-handlebars package for templating.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+
+import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
   root: 'src',
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src/partials'),
+    }),
+  ],
   build: {
     outDir: '../dist',
     emptyOutDir: true,
