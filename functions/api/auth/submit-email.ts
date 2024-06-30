955efa8feeed5a26e1c53284a6cb405b85e2716f
csterritt <csterritt@gmail.com>
2024-06-30 09:38:23 -0400

    Added page to let the user enter a code after email submit, first test.

--- functions/api/auth/submit-email.ts
+++ functions/api/auth/submit-email.ts
@@ -1,10 +1,12 @@
+import { StatusCodes } from 'http-status-codes'
+
 export const onRequest = async (context: any) => {
   const data = await context.request.formData()
   const email = data.get('email') || 'no email'
-  return Response.json({
-    status: 200,
-    body: {
-      message: `email is ${email}`,
-    },
-  })
+  console.log(`got email: ${email}`)
+  return Response.redirect(
+    // 'https://html-sign-in.pages.dev/auth/await-code.html', // PRODUCTION:UNCOMMENT
+    'http://localhost:3000/auth/await-code.html', // PRODUCTION:REMOVE
+    StatusCodes.MOVED_TEMPORARILY
+  )
 }
