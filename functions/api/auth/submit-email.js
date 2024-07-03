4c3c565ff2b9f25bfb3d064156558d7f9fcac894
csterritt <csterritt@gmail.com>
2024-07-03 19:23:59 -0400

    Added 'src' and 'functions' directories back into source control.

---
+++ functions/api/auth/submit-email.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var httpStatusCodes = require('http-status-codes');
+var cookie = require('cookie');
+
+const FUNCTION_SERVER_URL = 'http://localhost:8788'; // PRODUCTION:REMOVE
+// export const SERVER_URL = 'https://html-sign-in.pages.dev' // PRODUCTION:UNCOMMENT
+
+export const onRequest = async (context) => {  const data = await context.request.formData();
+  const email = data.get('email') || 'no email';
+
+  const cookies = cookie.serialize('email', email, {
+    httpOnly: true,
+    maxAge: 60 * 60, // 1 hour
+  });
+  const resp = new Response(null, {
+    headers: {
+      status: httpStatusCodes.StatusCodes.SEE_OTHER,
+      Location: `${FUNCTION_SERVER_URL}/api/auth/await-code`,
+      Refresh: `0; url=${FUNCTION_SERVER_URL}/api/auth/await-code`,
+      'Set-Cookie': cookies,
+    },
+  });
+
+  context.response = resp;
+
+  return resp
+};
+
