d1734b12785bf1c0170840c2cbbf6537eb580031
Chris Sterritt <csterritt@gmail.com>
2024-07-16 13:10:49 -0400

    Updated version to 4

--- src/partials/header.tsx
+++ src/partials/header.tsx
@@ -25,6 +25,6 @@ export const footer = () => (
   <div class='mx-6' data-testid='footer-banner'>
     <span>Content copyright Chris Sterritt, 2024</span>
     <span class='mx-2'>-</span>
-    <span>V-3</span>
+    <span>V-4</span>
   </div>
 )
