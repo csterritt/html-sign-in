b751f06d3b560e5d670972372044cb223645c429
csterritt <csterritt@gmail.com>
2024-07-16 13:18:27 -0400

    Corrected when timestamp-build-filenames.js runs.

--- do-prod-build.sh
+++ do-prod-build.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 rm -rf dist
 npx tailwindcss -m -o public/static/style-XXXXXX.css --content './src/**/*.{js,ts,jsx,tsx}'
 vite build
-node timestamp-build-filenames.js
 cp src/_headers src/_routes.json dist
 cp src/index.html dist
+node timestamp-build-filenames.js
