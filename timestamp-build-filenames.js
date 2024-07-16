4a3a386950d1a45b840e6b1d86a8766ef5a48497
csterritt <csterritt@gmail.com>
2024-07-16 11:08:40 -0400

    Debug prints in timestamp-build-filenames.js to help debug deploy prob.

--- timestamp-build-filenames.js
+++ timestamp-build-filenames.js
@@ -5,12 +5,12 @@ const distFilenameMatcher = /dist\/(.*?)XXXXXX(.*?)$/
 
 const buildPairs = (paths) => {
   const renamePairs = []
+  const now = new Date().getTime()
 
   paths.forEach((path) => {
     const match = distFilenameMatcher.exec(path)
     const prefix = match[1]
     const suffix = match[2]
-    const now = new Date().getTime()
     const renameTo = `${prefix}${now}${suffix}`
     fs.renameSync(path, 'dist/' + renameTo)
     renamePairs.push([`${prefix}XXXXXX${suffix}`, renameTo])
@@ -36,11 +36,17 @@ const fixFiles = (paths, pairs) => {
   })
 }
 
+console.log(`Entering timestamp-build-filenames.js`)
 let paths = await globby(['dist/**/*'])
+console.log(`... found ${paths.length} files in the project`)
 
 const pairs = buildPairs(
   paths.filter((filename) => filename.indexOf('XXXXXX') !== -1)
 )
 
+console.log(`... found ${pairs.length} pairs to rename`)
+
 paths = await globby(['dist/**/*'])
+console.log(`... about to fix files`)
 fixFiles(paths, pairs)
+console.log(`... done!`)
