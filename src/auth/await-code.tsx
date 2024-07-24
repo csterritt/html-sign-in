0b30c48b7c98fb09a2cfd523668508723e26dc3b
csterritt <csterritt@gmail.com>
2024-07-23 20:33:21 -0400

    Switched src/auth/await-code.tsx to use 'withSession'.

--- src/auth/await-code.tsx
+++ src/auth/await-code.tsx
@@ -6,11 +6,10 @@ import {
   AWAIT_CODE_PATH,
   EMAIL_SUBMITTED_COOKIE,
   ERROR_MESSAGE_COOKIE,
-  SESSION_COOKIE,
   SIGN_IN_PATH,
 } from '../constants'
 import { buildAwaitCodePage } from '../page-builders/build-await-code-page'
-import { getSessionInfoForSessionId } from '../db/session-db-access'
+import { withSession } from './with-session'
 
 export const setupAwaitCodePath = (app: HonoApp) => {
   app.get(AWAIT_CODE_PATH, async (c: LocalContext) => {
@@ -19,18 +18,13 @@ export const setupAwaitCodePath = (app: HonoApp) => {
       return c.redirect(SIGN_IN_PATH, StatusCodes.SEE_OTHER)
     }
 
-    const sessionId = getCookie(c, SESSION_COOKIE) ?? ''
-    if (sessionId.trim().length === 0) {
-      return c.redirect(SIGN_IN_PATH, StatusCodes.SEE_OTHER)
-    }
+    return await withSession(c, async (sessionIsValid) => {
+      if (!sessionIsValid) {
+        return c.redirect(SIGN_IN_PATH, StatusCodes.SEE_OTHER)
+      }
 
-    const sessionInfo = await getSessionInfoForSessionId(c, sessionId)
-    if (sessionInfo?.success !== true) {
-      // TODO: handle session not found
-      return c.redirect(SIGN_IN_PATH, StatusCodes.SEE_OTHER)
-    }
-
-    const errorMessage = getCookie(c, ERROR_MESSAGE_COOKIE) ?? ''
-    return buildAwaitCodePage(emailSubmitted, { error: errorMessage })(c)
+      const errorMessage = getCookie(c, ERROR_MESSAGE_COOKIE) ?? ''
+      return buildAwaitCodePage(emailSubmitted, { error: errorMessage })(c)
+    })
   })
 }
