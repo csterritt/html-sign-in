06cb6de37463049e9d1dafc1bad72daea2ca16a9
csterritt <csterritt@gmail.com>
2024-08-05 19:35:40 -0400

    Style fix for the background color.

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -2,13 +2,13 @@ import { jsxRenderer } from 'hono/jsx-renderer'
 
 export const renderer = jsxRenderer(({ children, title }) => {
   return (
-    <html>
+    <html class='bg-gray-300 dark:bg-gray-900'>
       <head>
         <link href='/static/style-XXXXXX.css' rel='stylesheet' />
         <title>HTML Sign In</title>
       </head>
 
-      <body class='font-slabserif h-full min-h-screen flex flex-col justify-between md:mx-auto max-w-7xl pb-3 bg-gray-300 dark:bg-gray-900'>
+      <body class='font-slabserif h-full min-h-screen flex flex-col justify-between md:mx-auto max-w-7xl pb-3'>
         {children}
       </body>
     </html>
