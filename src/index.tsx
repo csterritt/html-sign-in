0a3aab68fb5097906962f76a30e7da62884f8f7b
csterritt <csterritt@gmail.com>
2024-08-20 10:07:37 -0400

    Refactored out GetSession to its own file in the middleware directory.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import { Hono } from 'hono'
 import { LinearRouter } from 'hono/router/linear-router'
-import { createMiddleware } from 'hono/factory'
-import { getCookie } from 'hono/cookie'
-import Maybe, { nothing } from 'true-myth/maybe'
+import Maybe from 'true-myth/maybe'
 
 import { Bindings } from './bindings'
 import { renderer } from './renderer'
@@ -10,28 +8,14 @@ import { setup404Path } from './404'
 import { setupProtectedPath } from './protected'
 import { setupRootPath } from './root'
 import { setupSignInPaths } from './auth-paths'
-import {
-  SessionInformation,
-  getSessionInfoForSessionId,
-} from './db/session-db-access'
-import { SESSION_COOKIE } from './constants'
+import { SessionInformation } from './db/session-db-access'
+import { GetSession } from './middleware/get-session'
 
 declare module 'hono' {
   interface ContextVariableMap {
     Session: Maybe<SessionInformation>
   }
 }
-
-const GetSession = createMiddleware(async (c, next) => {
-  const sessionId = getCookie(c, SESSION_COOKIE) ?? ''
-  let sessionInfo: Maybe<SessionInformation> = nothing()
-  if (sessionId.trim().length > 0) {
-    sessionInfo = await getSessionInfoForSessionId(c, sessionId)
-  }
-
-  c.set('Session', sessionInfo)
-  await next()
-})
 
 const app: Hono<{ Bindings: Bindings }> = new Hono<{ Bindings: Bindings }>({
   router: new LinearRouter(),
---
+++ src/middleware/get-session.ts
@@ -0,0 +1,20 @@
+import { createMiddleware } from 'hono/factory'
+import { getCookie } from 'hono/cookie'
+import { SESSION_COOKIE } from '../constants'
+import Maybe, { nothing } from 'true-myth/maybe'
+import {
+  getSessionInfoForSessionId,
+  SessionInformation,
+} from '../db/session-db-access'
+import { LocalContext } from '../bindings'
+
+export const GetSession = createMiddleware(async (c: LocalContext, next) => {
+  const sessionId = getCookie(c, SESSION_COOKIE) ?? ''
+  let sessionInfo: Maybe<SessionInformation> = nothing()
+  if (sessionId.trim().length > 0) {
+    sessionInfo = await getSessionInfoForSessionId(c, sessionId)
+  }
+
+  c.set('Session', sessionInfo)
+  await next()
+})
